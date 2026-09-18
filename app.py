270a4be6a334564b06c0af330ef40214aada6b64
Salman Faris <sf8990@srmist.edu.in>
2026-09-18 16:54:39 +0530

    Prep for Render deploy

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import json
 import random
 import sqlite3
+import os
 
 app = Flask(__name__)
 DECK_FILE = "deck.json"
@@ -106,4 +107,5 @@ def answer():
     
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = "0.0.0.0", port=port)    
